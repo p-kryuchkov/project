fa97bc5f6006394c7d0bd539f905640a498e77ca
Pavel Kryuchkov <pavel.kryuchkov@appline.ru>
2021-02-08 10:27:24 +0300

    Исправление ошибок в практическом задании на массивы

--- src/programArray.java
+++ src/programArray.java
@@ -1,13 +1,12 @@
 public class programArray {
     public static void main(String[] args) {
-        int[] myArray = {11, 23, 31, 44, 58,};
+        int[] myArray = {11, 23, 31, 44, 58};
         int a = myArray[0];
-        int b = myArray[myArray.length - 1];
-        System.out.println(myArray[0] +" " + myArray[myArray.length - 1]);
-         myArray[0] = b;
-         myArray[myArray.length - 1] = a;
-        System.out.println(myArray[0] +" " + myArray[myArray.length - 1]);
-        int c = (myArray.length - 1) / 2 ; 
+        System.out.println(myArray[0] + " " + myArray[myArray.length - 1]);
+        myArray[0] = myArray[myArray.length - 1];
+        myArray[myArray.length - 1] = a;
+        System.out.println(myArray[0] + " " + myArray[myArray.length - 1]);
+        int c = (myArray.length - 1) / 2 ;
         System.out.println(myArray[0] + myArray[c]);
 
     }

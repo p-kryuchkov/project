1fce6406491f0db03a68ab2ff35ced8b2660ba88
Pavel Kryuchkov <pavel.kryuchkov@appline.ru>
2021-02-17 00:38:12 +0300

    Правка для Итоговая задача №1

--- src/itog1.java
+++ src/itog1.java
@@ -8,8 +8,13 @@ public class itog1 {
         Scanner cr = new Scanner(System.in);
         System.out.println("Введите количество рублей: ");
         double r = cr.nextDouble();
-        double result = Math.round((r / c) * 100);
-        System.out.println( "Итого: " + (result / 100) + " долларов") ;
+        if (r > 0 & c > 0) {
+            double result = Math.round((r / c) * 100);
+            System.out.println("Итого: " + (result / 100) + " долларов");
+        }
+        else {
+            System.out.println("Введите корректные данные");
+        }
     }
 
 }
